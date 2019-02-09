76f8856ee5ef0f96fc01326206ed2dc96248877e
Michael Swierszcz <michaelswierszcz@gmail.com>
2019-02-08 19:07:22 -0500

    mac_push

--- Django/Django-Python-Full-Stack-Web-Devloper-master/Advanced_Django_CBV/advcbv/basic_app/views.py
+++ Django/Django-Python-Full-Stack-Web-Devloper-master/Advanced_Django_CBV/advcbv/basic_app/views.py
@@ -20,16 +20,17 @@ class IndexView(TemplateView):
     # template_name = 'app_name/site.html'
     template_name = 'index.html'
 
+    '''
     def get_context_data(self,**kwargs):
         context  = super().get_context_data(**kwargs)
         context['injectme'] = "Basic Injection!"
         return context
-
+    '''
 
 class SchoolListView(ListView):
     context_object_name = 'schools'
     model = models.School
-    template_name = 'basic_app/school_list.html'
+    #template_name = 'basic_app/school_list.html'
 
 
 class SchoolDetailView(DetailView):
